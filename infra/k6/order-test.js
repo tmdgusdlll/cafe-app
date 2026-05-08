e79609af2530f20b344c8b20c6034eb696f7100a
tmdgsudl <leesh27955@gmail.com>
2026-05-08 20:15:39 +0900

    feature: 단일 vs 다중 서버 비교 테스트

--- infra/k6/order-test.js
+++ infra/k6/order-test.js
@@ -2,11 +2,13 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-    stages: [
-        { duration: '30s', target: 50 },  // 워밍업 (적은 사용자로 시작)
-        { duration: '1m', target: 50 },  // 본 테스트
-        { duration: '30s', target: 0 },   // 종료
-    ],
+    // stages: [
+    //     { duration: '30s', target: 50 },  // 워밍업 (적은 사용자로 시작)
+    //     { duration: '1m', target: 50 },  // 본 테스트
+    //     { duration: '30s', target: 0 },   // 종료
+    // ],
+    vus: 100,
+    duration: '10s',
     thresholds: {
         http_req_failed: ['rate<0.05'],
         http_req_duration: ['p(95)<500'],
@@ -18,6 +20,8 @@ export default function () {
     // const url = 'http://nginx/users/1/order';
     // 분산 락
     const url = 'http://nginx/users/1/order/redisson';
+    // 단일 인스턴스
+    // const url = 'http://host.docker.internal:8080/users/1/order/redisson';
 
     const payload = JSON.stringify({
         menuId: 1,
